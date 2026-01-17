1594505e83be6290cb1c4d6a16447622c2d24f2a
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-01-17 17:09:50 +0200

    with key frames

--- src/gsap/KeyFrames.jsx
+++ src/gsap/KeyFrames.jsx
@@ -5,24 +5,42 @@ import { useGSAP } from "@gsap/react";
 
 const KeyFrames = () => {
   useGSAP(() => {
-    const tl = gsap
-      .timeline()
-      .to("span", {
-        x: 200,
-        duration: 1,
-      })
-      .to("span", {
-        y: 200,
-        duration: 1,
-      })
-      .to("span", {
-        x: 0,
-        duration: 1,
-      })
-      .to("span", {
-        y: 0,
-        duration: 1,
-      });
+    // const tl = gsap
+    //   .timeline()
+    //   .to("span", {
+    //     x: 200,
+    //     duration: 1,
+    //   })
+    //   .to("span", {
+    //     y: 200,
+    //     duration: 1,
+    //   })
+    //   .to("span", {
+    //     x: 0,
+    //     duration: 1,
+    //   })
+    //   .to("span", {
+    //     y: 0,
+    //     duration: 1,
+    //   });
+
+    // using keyFrames
+
+    gsap.to("span", {
+      keyframes: [
+        { x: 100 },
+        { y: 100 },
+        {
+          x: 0,
+          delay: 2,
+          onComplete: () => {
+            console.log("third completed");
+          },
+        },
+        { y: 0 },
+        { x: 100 },
+      ],
+    });
   }, {});
 
   return (
