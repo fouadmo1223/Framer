2474e4b2d30b759f99484ff2565dc2e644367903
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-01-17 13:47:29 +0200

    animation on words and text

--- src/gsap/plugins/SplitTextDemo.jsx
+++ src/gsap/plugins/SplitTextDemo.jsx
@@ -8,12 +8,32 @@ import { useGSAP } from "@gsap/react";
 const SplitTextDemo = () => {
   useGSAP(() => {
     const split = SplitText.create("span", {
-      type: "lines",
+      type: "lines,chars",
       linesClass: "line++", // each line take diff class
     });
 
-    // in js 
+    // in js
     // document.fonts.ready  to wait fonts to be loadded
+
+    // make animation on splited text
+    // gsap.from(split.lines, {
+    //   opacity: 0,
+    //   y: 100,
+    //   stagger: 1,
+    //   duration: 1,
+    //   ease: "power2.out",
+    // });
+
+    gsap.from(split.chars, {
+      opacity: 0,
+      y: 100,
+      stagger: {
+        each: "0.05",
+        from: "random",
+      },
+      duration: 1,
+      ease: "elastic.out",
+    });
   }, []);
 
   return (
@@ -21,7 +41,7 @@ const SplitTextDemo = () => {
       id="smooth-content"
       className="px-6 h-full w-full flex justify-center items-center"
     >
-      <span className="text-6xl text-center">
+      <span className="text-4xl text-center">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam odit
         laudantium hic nobis amet! Vitae, nihil, neque quisquam fugit nobis sed
         consequuntur voluptatem mollitia excepturi cupiditate odit alias facere
