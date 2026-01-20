004360d4b1641f5225c66086cf0b69f551db60ed
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-01-20 21:06:20 +0200

    make action when scroll

--- src/gsap/plugins/ObserverPractice.jsx
+++ src/gsap/plugins/ObserverPractice.jsx
@@ -8,15 +8,34 @@ gsap.registerPlugin(Observer, SplitText);
 
 const ObserverPractice = () => {
   useGSAP(() => {
+    // Observer.create({
+    //   target: ".selected",
+    //   onClick: () => console.log("clicked"),
+    // });
+
+    function rotate(value) {
+      gsap.to(".selected", {
+        rotation: value,
+        duration: 0.5,
+        ease: "power2.inOut",
+        transformOrigin: "center",
+        overwrite: true,
+      });
+    }
+
     Observer.create({
-      target: ".selected",
-      onClick: () => console.log("clicked"),
+      target: "window",
+      type: "wheel,touch,scroll,pointer",
+      onDown: () => rotate(0),
+      onUp: () => rotate(180),
     });
   });
 
   return (
-    <div className="relative w-full h-screen overflow-hidden">
-      <div className="w-18 h-18 selected rounded-full mt-20 mx-auto bg-green-500"></div>
+    <div className="relative w-full h-screen flex justify-center items-center">
+      <div className="w-18 h-18 flex justify-center items-center selected rounded-full  mx-auto bg-green-500 text-2xl">
+        F
+      </div>
     </div>
   );
 };
