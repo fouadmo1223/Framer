00a51f255bc7fd43b1d5d73b00d3a249855bb534
LAPTOP-I033L8EJ\abdo1 <fm0850020@gmail.com>
2026-01-30 16:21:40 +0200

    make box openening animation

--- src/gsap/examples/Opening.jsx
+++ src/gsap/examples/Opening.jsx
@@ -9,7 +9,7 @@ const OverLay = ({ timeline }) => {
   useGSAP(
     () => {
       timeline &&
-        gsap.to("div", {
+        timeline.to("div", {
           xPercent: gsap.utils.wrap([-100, 100]),
           duration: 2,
           ease: "power2.inOut",
@@ -37,17 +37,30 @@ const OverLay = ({ timeline }) => {
 const Opening = () => {
   const container = useRef(null);
   const timeline = gsap.timeline();
+  const hero = useRef(null);
 
-  useGSAP(() => {}, { scope: container });
+  useGSAP(
+    () => {
+      timeline.from(hero.current, {
+        clipPath: "inset(20% 20% 20% 20%)",
+        duration: 2,
+        ease: "power2.inOut",
+      });
+    },
+    { scope: container },
+  );
 
   return (
     <>
       <OverLay timeline={timeline} />
       <div
         ref={container}
-        className=" w-full h-[90%] text-white text-[180px] pt-10 flex flex-col items-center justify-center  "
+        className=" w-full h-[90%] bg-white text-white text-[180px] pt-10 flex flex-col items-center justify-center  "
       >
-        <div className="w-[90%] relative   h-full overflow-hidden flex items-center justify-center">
+        <div
+          ref={hero}
+          className="w-[100%] bg-black relative   h-full overflow-hidden flex items-center justify-center"
+        >
           <div className="">
             {/* // don't make animation on element have css styles for x and y */}
             <h2>GSAP</h2>
